949a966e32835a9a54a92aa66fbd39a396c0165b
Idris Olalekan <105294871+AEedriz@users.noreply.github.com>
2022-05-28 02:33:23 -0700

    Update main.py

--- main.py
+++ main.py
@@ -3,10 +3,8 @@
 # count_words("The cake is done. It is a big cake!")
 # --> {"cake":2, "big:1":1, "the":1, "a":1, "it":1}
 
-from asyncio import coroutines
 from asyncore import read
 from itertools import count
-from unittest.util import _count_diff_hashable
 
 def read_file_content(filename):
     # [assignment] Add your code here
